87d6f367a81e0272a80947c85859c79be188de2a
stojanovic98m <stojanovic98m@gmail.com>
2022-08-23 13:17:02 +0200

    removed unused icons &  crosshair icon updated

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,5 @@
-import crayon from "./crayon.png";
+import pen from "./pen.png";
 import dragon_example from "./dragon-example.png";
-import pencil from "./pencil.png";
 import title from "./title.png";
 
-export { crayon, dragon_example, pencil, title };
+export { pen, dragon_example, title };
