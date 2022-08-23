c0b4b83fdde09607a4054c72ea3c9e5b83df3196
stojanovic98m <stojanovic98m@gmail.com>
2022-08-23 13:02:31 +0200

    added crayons

---
+++ src/images/crayons/index.js
@@ -0,0 +1,23 @@
+import black from "./black.png";
+import blue from "./blue.png";
+import red from "./red.png";
+import brown from "./brown.png";
+import orange from "./orange.png";
+import yellow from "./yellow.png";
+import purple from "./purple.png";
+import lightblue from "./lightblue.png";
+import pink from "./pink.png";
+import green from "./green.png";
+
+export {
+  black,
+  blue,
+  red,
+  brown,
+  orange,
+  yellow,
+  purple,
+  lightblue,
+  pink,
+  green,
+};
--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,6 @@
-import crayon50 from "./crayon50.png";
 import crayon from "./crayon.png";
 import dragon_example from "./dragon-example.png";
 import pencil from "./pencil.png";
 import title from "./title.png";
 
-export { crayon50, crayon, dragon_example, pencil, title };
+export { crayon, dragon_example, pencil, title };
